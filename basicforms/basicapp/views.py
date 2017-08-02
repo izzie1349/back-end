1bed1513218c97c9d130eb680bc72752d4b64b8e
izzie1349 <izzie1349@gmail.com>
2017-08-02 16:40:51 -0400

    django: forms: exploring userinput/POST request

--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -7,4 +7,15 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    # Simple example getting user data
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            # do something
+            print('validation success from POST')
+            print('NAME => ', form.cleaned_data['name'])
+            print('EMAIL => ', form.cleaned_data['email'])
+            print('TEXT => ', form.cleaned_data['text'])
+
     return render(request, 'basicapp/form_page.html', { 'form':form })
