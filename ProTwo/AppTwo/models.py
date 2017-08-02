35f621eedfa2788ef5212a7f5d5ff46a03aeb4e9
izzie1349 <izzie1349@gmail.com>
2017-08-02 12:17:50 -0400

    django: mtv: create model, run migrations, register model in admin

--- ProTwo/AppTwo/models.py
+++ ProTwo/AppTwo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return  self.email
