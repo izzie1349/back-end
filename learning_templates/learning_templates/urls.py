24ce425f2423df3289ebbedb1f6c8fab12130d1c
izzie1349 <izzie1349@gmail.com>
2017-08-04 13:41:59 -0400

    django: map urls to templates

--- learning_templates/learning_templates/urls.py
+++ learning_templates/learning_templates/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from basic_app import views
 
 urlpatterns = [
+    # points to view method
+    url(r'^$', views.index, name='index'),
+    # points to url in basic_app
+    url(r'^basic_app/', include('basic_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
