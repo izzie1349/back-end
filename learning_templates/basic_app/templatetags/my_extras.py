0d46c8ae605c749a188f79540819f15d18afb2d9
izzie1349 <izzie1349@gmail.com>
2017-08-05 14:38:31 -0400

    django: custom templatetag filters

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+def custom_built_cut_filter(value, arg):
+    """
+    This cuts out all values of 'arg' from the string
+    """
+    # ASSUMPTION: value is a string
+    return value.replace(arg, '')
+
+# arg1: string used to call filter in the template tag
+# arg2: call the actual function
+register.filter('cut_custom', custom_built_cut_filter)
