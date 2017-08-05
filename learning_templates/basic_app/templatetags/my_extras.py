fb2c9b83c97e80bce5fc05b099d1596a6701fa08
izzie1349 <izzie1349@gmail.com>
2017-08-05 14:58:06 -0400

    django: templatetag filters using decorators

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='cut_custom')
 def custom_built_cut_filter(value, arg):
     """
     This cuts out all values of 'arg' from the string
@@ -9,6 +10,6 @@ def custom_built_cut_filter(value, arg):
     # ASSUMPTION: value is a string
     return value.replace(arg, '')
 
-# arg1: string used to call filter in the template tag
-# arg2: call the actual function
-register.filter('cut_custom', custom_built_cut_filter)
+# # arg1: string used to call filter in the template tag
+# # arg2: call the actual function
+# register.filter('cut_custom', custom_built_cut_filter)
