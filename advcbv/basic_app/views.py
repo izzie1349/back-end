6b076ee09fdeaf32db2059030d069ae1ba36f559
izzie1349 <izzie1349@gmail.com>
2017-08-08 01:56:37 -0400

    django: cbv: basic injection w templateviews

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -15,3 +15,8 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION'
+        return context
