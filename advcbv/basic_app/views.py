fa9cd12b146d0e881e8ac06f09103ad96d50a124
izzie1349 <izzie1349@gmail.com>
2017-08-12 15:57:40 -0400

    django: delete: urls views template to delete school

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import (View, TemplateView, ListView, DetailView,
                                   CreateView, UpdateView, DeleteView)
 from django.http import HttpResponse
@@ -42,3 +43,7 @@ class SchoolCreateView(CreateView):
 class SchoolUpdateView(UpdateView):
     fields = ('name', 'principal')
     model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
